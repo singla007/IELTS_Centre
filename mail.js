2e79b4259a01c61beb6404a7637a77dfbb56b6b8
Yogesh Singla <eryogeshkumarsingla007@gmail.com>
2023-01-08 00:22:04 -0500

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,58 @@
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNzQX7Hh313VuQATCYWEla9G57mZMfSx8",
+    authDomain: "ielts-coaching-db.firebaseapp.com",
+    databaseURL: "https://ielts-coaching-db-default-rtdb.firebaseio.com",
+    projectId: "ielts-coaching-db",
+    storageBucket: "ielts-coaching-db.appspot.com",
+    messagingSenderId: "829784358227",
+    appId: "1:829784358227:web:49ca95c77e14618f45b5a6",
+    measurementId: "G-BQKXLWCK5C"
+
+  };
+  
+  // initialize firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // reference your database
+  var contactFormDB = firebase.database().ref("contactForm");
+  
+  document.getElementById("contactForm").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+  
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var msgContent = getElementVal("msgContent");
+    var phone = getElementVal("phone");
+  
+    saveMessages(name, emailid, phone, msgContent);
+  
+    //   enable alert
+    document.querySelector(".alert").style.display = "block";
+  
+    //   remove the alert
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 3000);
+  
+    //   reset the form
+    document.getElementById("contactForm").reset();
+  }
+  
+  const saveMessages = (name, emailid, phone, msgContent) => {
+    var newContactForm = contactFormDB.push();
+  
+    newContactForm.set({
+      name: name,
+      emailid: emailid,
+      phone: phone,
+      msgContent: msgContent,
+    });
+  };
+  
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
